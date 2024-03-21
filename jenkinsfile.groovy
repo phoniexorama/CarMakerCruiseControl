637b06fa90d317fb6734be987bc88d9f1f8febc9
Keerthi SREERANGAIAH (FERCHAU Automotive) <keerthi.sreerangaiah@ferchau.com>
2024-03-21 09:55:05 +0100

    Files added

---
+++ jenkinsfile.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    environment {
+        TEMPLATE_FOLDER_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\Data\\TestRun\\Lenkwinkelrampe_Template"
+        VEHICLE_FOLDER_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\Data\\Vehicle"
+        OUTPUT_FOLDER = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\SimOutput\\ENGPMAKNB022"
+        LOG_FOLDER = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\SimOutput\\ENGPMAKNB022\\Log"
+        VFF_FOLDER_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\Data\\Vehicle"
+        DAT_FOLDER_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\SimOutput\\ENGPMAKNB022\\log"
+        EXCEL_FOLDER_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\VehicleInfoExcel"
+        BATCH_SCRIPT_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\carmakerTestseries.bat"
+        TEST_SERIES_FOLDER_PATH = "C:\\CM_Test\\Frg-Bedatung_Cayenne_E4_CM12\\Testseries"
+    }
+
+    stages {
+        stage('dat file generation') {
+            steps {
+                script {
+                    // Call the Python script for dat file generation
+                    bat "python carmakerdatfilegenerator.py"
+                }
+            }
+        }
+
+        stage('excel file generator') {
+            steps {
+                script {
+                    // Call the Python script for excel file generation
+                    bat "python autoexcelfilegenerator.py"
+                }
+            }
+        }
+
+        stage('test series generator') {
+            steps {
+                script {
+                    // Call the Python script for test series generation
+                    bat "python testseriesgenerator.py"
+                }
+            }
+        }
+
+        stage('run test manager') {
+            steps {
+                script {
+                    // Call the Python script for running test manager
+                    bat "python runtestmanager.py"
+                }
+            }
+        }
+    }
+}
---
+++ runTestManager.py
@@ -0,0 +1,61 @@
+import subprocess
+import time
+import os
+import re
+
+def replace_tsfname_in_batch_script(batch_script_path, new_tsfname):
+    try:
+        # Read the contents of the batch script
+        with open(batch_script_path, 'r') as file:
+            batch_script_content = file.readlines()
+
+        # Find and replace the value of TSFNAME
+        for i, line in enumerate(batch_script_content):
+            match = re.match(r'^(\s*SET\s+TSFNAME\s*=\s*)\s*(.*?)\s*$', line)
+            if match:
+                # Update the line with the new TSFNAME value while preserving formatting
+                batch_script_content[i] = f"{match.group(1)}{new_tsfname}\n"
+                time.sleep(20)
+                break
+
+        # Write the modified content back to the file
+        with open(batch_script_path, 'w') as file:
+            file.writelines(batch_script_content)
+
+        print(f"Value of TSFNAME replaced with '{new_tsfname}' in the batch script.")
+    except FileNotFoundError:
+        print(f"Error: Batch script '{batch_script_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def run_batch_script(script_path):
+    try:
+        # Add delay of 1 minute before running the batch script
+        print("Waiting for 30 sec before executing the batch script...")
+        time.sleep(30)
+
+        process = subprocess.Popen(script_path, shell=True)
+        print("Batch script executed.")
+        # Wait for the CM.exe process to finish
+        while process.poll() is None:
+            time.sleep(1)  # Sleep for 1 second
+        print("CM.exe process has finished.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    batch_script_path = "C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/carmakerTestseries.bat"
+    folder_path = "C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/Data/TestRun"
+
+    # Get a list of all .ts files in the directory
+    ts_files = [filename for filename in os.listdir(folder_path) if filename.endswith(".ts")]
+
+    for ts_file in ts_files:
+        ts_file_path = os.path.join(folder_path, ts_file)
+
+        # Replace TSFNAME in the batch script with the current ts file name
+        replace_tsfname_in_batch_script(batch_script_path, ts_file)
+
+        # Execute the modified batch script
+        run_batch_script(batch_script_path)
